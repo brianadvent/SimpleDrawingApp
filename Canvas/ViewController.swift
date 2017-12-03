343f0fd79f821e2f53222c7886b55d855af16cfb
Brian Advent <youtube@brianadvent.com>
2017-12-03 15:36:36 +0100

    Adding missing files

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var canvasView: CanvasView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func clearCanvas(_ sender: Any) {
+        canvasView.clearCanvas()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
